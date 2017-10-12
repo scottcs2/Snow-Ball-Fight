1a053de1e2c9daff1897e1400d4afabe78aa2302
ssscrazy <ssscrazy@live.com>
2017-10-11 20:58:06 -0400

    Update SaveInventory.java
    Added comments for clarity

--- SaveInventory.java
+++ SaveInventory.java
@@ -6,34 +6,36 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class SaveInventory {
-	private HashMap<String, ItemStack> originalInventory = new HashMap<String, ItemStack>();
-	private Player p;
-	public SaveInventory(Player player) {
+public class SaveInventory { // Temporarily stores a player's inventory in this plugin's memory while the player is in a snowball match
+			     // This allows for players to play the normal game of Minecraft and join/leave arenas as they please.
+			     // i.e. the player does not need to worry about losing their items while in this special gamemode.
+	private HashMap<String, ItemStack> originalInventory = new HashMap<String, ItemStack>(); // Create a list of the player's items.
+	private Player p; 
+	public SaveInventory(Player player) { // This function stores the inventory of the player that was passed into the function.
 		p = player;
-		Inventory inventory = player.getInventory();
+		Inventory inventory = player.getInventory(); // Get their entire inventory.
 
-		for (int i = 0; i < inventory.getSize(); i++) {
-			ItemStack item = inventory.getItem(i);
-			if(item != null)
-			originalInventory.put("s" + i, item.clone());
+		for (int i = 0; i < inventory.getSize(); i++) { // Cycle through all the potential item slots in their inventory
+			ItemStack item = inventory.getItem(i); // Get the item in that slot
+			if(item != null) // If it wasn't empty,
+			originalInventory.put("s" + i, item.clone()); // Get a copy of the item and put it into our saved inventory.
 		}
-		if(player.getInventory().getHelmet() != null)
-			originalInventory.put("Helmet", player.getInventory().getHelmet().clone());
-		if(player.getInventory().getChestplate() != null)
+		if(player.getInventory().getHelmet() != null) // Check if the player is wearing a helmet.
+			originalInventory.put("Helmet", player.getInventory().getHelmet().clone()); // If so, store the helmet.
+		if(player.getInventory().getChestplate() != null) // Same as above
 			originalInventory.put("Chestplate", player.getInventory().getChestplate().clone());
-		if(player.getInventory().getLeggings() != null)
-			originalInventory.put("Leggings", player.getInventory().getLeggings().clone());
+		if(player.getInventory().getLeggings() != null) 
+			originalInventory.put("Leggings", player.getInventory().getLeggings().clone()); 
 		if(player.getInventory().getBoots() != null)
 			originalInventory.put("Boots", player.getInventory().getBoots().clone());
 		
 		
 	}
-	public void Load() {
-		for (int i = 0; i < p.getInventory().getSize(); i++)  {
+	public void Load() { // This is called when we return the player's inventory after they leave a match.
+		for (int i = 0; i < p.getInventory().getSize(); i++)  { // Set their items back to how they were, in the same spots as before.
 			p.getInventory().setItem(i, originalInventory.get("s" + i));
 		}
-		p.getInventory().setHelmet(originalInventory.get("Helmet"));
+		p.getInventory().setHelmet(originalInventory.get("Helmet")); // Put their armor back on as well.
 		p.getInventory().setChestplate(originalInventory.get("Chestplate"));
 		p.getInventory().setLeggings(originalInventory.get("Leggings"));
 		p.getInventory().setBoots(originalInventory.get("Boots"));
